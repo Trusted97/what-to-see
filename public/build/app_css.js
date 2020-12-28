47cd8f9c1df2c65a8cd2459b7a1dc7b5fb76a40b
Trusted97 <gianlucabenucci9700@gmail.com>
2020-12-28 15:58:45 +0100

    Maledetto .gitignore

---
+++ public/build/app_css.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["app_css"],{
+
+/***/ "./assets/styles/app.css":
+/*!*******************************!*\
+  !*** ./assets/styles/app.css ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+},[["./assets/styles/app.css","runtime"]]]);
